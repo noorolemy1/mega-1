0c8eb02fda7690a04cd0e5e620fda943cd4c6c4f
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2022-04-11 23:04:24 +0200

    mega

---
+++ lib/mega_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MegaScreen extends StatelessWidget {
+  const MegaScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
