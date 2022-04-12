250b554fc9532a2ff36826b71f93de92c8533e1a
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2022-04-12 18:22:18 +0200

    mega

--- lib/mega_screen.dart
+++ lib/mega_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
-
 class MegaScreen extends StatefulWidget {
   const MegaScreen({Key key}) : super(key: key);
 
@@ -13,7 +11,9 @@ class MegaScreen extends StatefulWidget {
 }
 
 class _MegaScreenState extends State<MegaScreen> {
-  bool isSwitch = false;
+  bool isSwitchDoor = false;
+  bool smoke = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +30,6 @@ class _MegaScreenState extends State<MegaScreen> {
         ),
       ),
       body: Column(
-        //mainAxisAlignment: MainAxisAlignment.start,
         children: [
           const Center(
             child: Text(
@@ -38,82 +37,203 @@ class _MegaScreenState extends State<MegaScreen> {
               style: TextStyle(fontSize: 50),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10, top: 50),
-                child: Stack(
-                  children: [
-                    Container(
-                      height: 210,
-                      width: 170,
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, top: 30),
-                      child: Column(
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            textBaseline: TextBaseline.alphabetic,
-                            children: const [
-                              Icon(Icons.door_front_door,
-                                  color: Colors.white, size: 50),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10, top: 50),
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: 210,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, top: 30),
+                          child: Column(
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                textBaseline: TextBaseline.alphabetic,
+                                children: const [
+                                  Icon(Icons.door_front_door,
+                                      color: Colors.white, size: 50),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 15),
+                                    child: Text(
+                                      'Door',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 30),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              if (isSwitchDoor == false)
+                                Row(
+                                  children: const [
+                                    CircleAvatar(
+                                      backgroundColor: Colors.green,
+                                      radius: 5,
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      'Normall',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    ),
+                                  ],
+                                ),
+                              if (isSwitchDoor == true)
+                                Row(
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundColor: Colors.redAccent[700],
+                                      radius: 5,
+                                    ),
+                                    const SizedBox(
+                                      width: 15,
+                                    ),
+                                    const Text(
+                                      'close',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    ),
+                                  ],
+                                ),
                               Padding(
-                                padding: EdgeInsets.only(left: 15),
-                                child: Text(
-                                  'Door',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 30),
+                                padding:
+                                    const EdgeInsets.only(top: 15, left: 30),
+                                child: Row(
+                                  children: [
+                                    CustomSwitch(
+                                      value: isSwitchDoor,
+                                      activeColor: Colors.blue,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          isSwitchDoor = value;
+                                        });
+                                        print(value);
+                                      },
+                                    ),
+                                  ],
                                 ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            children: const [
-                              CircleAvatar(
-                                backgroundColor: Colors.green,
-                                radius: 5,
-                              ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Text(
-                                'Normall',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 25),
                               ),
                             ],
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10,left: 30),
-                            child: Row(
-                              children: [
-                                CustomSwitch(
-                                  value: isSwitch,
-                                  activeColor: Colors.blue,
-                                  onChanged: (value){
-                                    setState(() {
-                                      isSwitch = value;
-                                    });
-                                    print(value);
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10, top: 50),
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: 210,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, top: 30),
+                          child: Column(
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                textBaseline: TextBaseline.alphabetic,
+                                children: const [
+                                  Icon(Icons.door_front_door,
+                                      color: Colors.white, size: 50),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 15),
+                                    child: Text(
+                                      'Door',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 30),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              if (smoke == false)
+                                Row(
+                                  children: [
+                                    const CircleAvatar(
+                                      backgroundColor: Colors.green,
+                                      radius: 5,
+                                    ),
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                    const Text(
+                                      'Normall',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    ),
+                                  ],
+                                ),
+                              if (smoke == true)
+                                Row(
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundColor: Colors.redAccent[700],
+                                      radius: 5,
+                                    ),
+                                    const SizedBox(
+                                      width: 15,
+                                    ),
+                                    const Text(
+                                      'close',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    ),
+                                  ],
+                                ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 15, left: 30),
+                                child: Row(
+                                  children: [
+                                    CustomSwitch(
+                                      value: smoke,
+                                      activeColor: Colors.blue,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          smoke = value;
+                                        });
+                                        print(value);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
